b49e9eee9afe9673afb970900cd4dd2e927f73dd
ft-Ali <alsiavos@student.42.fr>
2024-10-30 13:20:29 +0100

    parsing done/replace fix

--- srcs/checker/check_valid.c
+++ srcs/checker/check_valid.c
@@ -6,7 +6,7 @@
 /*   By: alsiavos <alsiavos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 13:01:56 by alsiavos          #+#    #+#             */
-/*   Updated: 2024/10/30 13:09:19 by alsiavos         ###   ########.fr       */
+/*   Updated: 2024/10/30 13:18:56 by alsiavos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ static bool	is_space(char c)
 	return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f'
 		|| c == '\r');
 }
-int isstartorground(char c)
+int	isstartorground(char c)
 {
-    return (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W');
+	return (c == '0' || c == 'N' || c == 'S' || c == 'E' || c == 'W');
 }
-
 
 int	ft_sstrlen(char *str)
 {
@@ -60,24 +59,27 @@ void	check_char(t_game *game)
 	}
 }
 
-void map_height_valid(t_game *game)
+void	map_height_valid(t_game *game)
 {
-    int i = 0;
-    int j = 0;
-    while (game->map.grid[i])
-    {
-        j = 0;
-        while (game->map.grid[i][j])
-        {
-            j++;
-        }
-        if (j > game->map.width)
-            game->map.width = j;
-        i++;
-    }
-    game->map.height = i;
-    if (game->map.height < 3 || game->map.width < 3)
-        handle_error(game, ERR_MAP_TOO_SMALL);
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (game->map.grid[i])
+	{
+		j = 0;
+		while (game->map.grid[i][j])
+		{
+			j++;
+		}
+		if (j > game->map.width)
+			game->map.width = j;
+		i++;
+	}
+	game->map.height = i;
+	if (game->map.height < 3 || game->map.width < 3)
+		handle_error(game, ERR_MAP_TOO_SMALL);
 }
 
 void	check_one_player(t_game *game)
@@ -103,8 +105,8 @@ void	check_one_player(t_game *game)
 		}
 		i++;
 	}
-    if(game->player.pos_x == 0 || game->player.pos_y == 0)
-        handle_error(game, ERR_PLAYER_DIR);
+	if (game->player.pos_x == 0 || game->player.pos_y == 0)
+		handle_error(game, ERR_PLAYER_DIR);
 }
 
 void	check_enclosure_border(t_game *game)
@@ -135,16 +137,16 @@ void	check_enclosure_side(t_game *game)
 	int	i;
 	int	j;
 
-	i = 0;
-	while (game->map.grid[i])
+	i = -1;
+	while (game->map.grid[++i])
 	{
 		j = 0;
 		while (game->map.grid[i][j])
 		{
 			if (isstartorground(game->map.grid[i][j]))
 			{
-				if (i == 0 || j == 0 || game->map.grid[i + 1] == NULL || 
-					j + 1 >= ft_sstrlen(game->map.grid[i]))
+				if (i == 0 || j == 0 || game->map.grid[i + 1] == NULL || j
+					+ 1 >= ft_sstrlen(game->map.grid[i]))
 				{
 					handle_error(game, ERR_MAP_NO_WALLS);
 				}
@@ -152,16 +154,12 @@ void	check_enclosure_side(t_game *game)
 					game->map.grid[i + 1][j] == ' ' || // Case du bas
 					game->map.grid[i][j - 1] == ' ' || // Case de gauche
 					game->map.grid[i][j + 1] == ' ')   // Case de droite
-				{
-					 map_replace(game);
-				}
+					map_replace(game);
 			}
 			j++;
 		}
-		i++;
 	}
 }
-
 
 void	map_replace(t_game *game)
 {
@@ -188,7 +186,7 @@ void	check_map_valid(t_game *game)
 
 	check_char(game);
 	check_one_player(game);
-    map_height_valid(game);
+	map_height_valid(game);
 	check_enclosure_border(game);
 	check_enclosure_side(game);
 	i = 0;
